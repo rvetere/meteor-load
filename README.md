d5e2e08b5f14b97c8afe20edd7202ab9f17bb7b7
Ben Wiley <ben.wiley@newspring.cc>
2015-02-11 09:53:57 -0500

    Port code

--- README.md
+++ README.md
@@ -1,1 +1,87 @@
 # meteor-pkg-get-all-files
+
+A Meteor package builder's helper to add all files within your package in a
+Meteor-esque manner
+
+## Example usage
+
+Consider the following sample package. The package.js is the following:
+
+````javascript
+Package.describe({
+  name: "coolguy22:some-package",
+  version: '1.2.3',
+  summary: 'A summary here',
+  git: 'The git url goes here',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.3.1');
+
+  api.addFiles('lib/client/a.js', 'client');
+  api.addFiles('lib/client/z.js', 'client');
+  api.addFiles('lib/client/main.js', 'client');
+
+  api.addFiles('lib/server/a.js', 'server');
+  api.addFiles('lib/server/z.js', 'server');
+  api.addFiles('lib/server/main.js', 'server');
+
+  api.addFiles('lib/a.js');
+  api.addFiles('lib/z.js');
+  api.addFiles('lib/main.js');
+
+  api.addFiles('client/a.js', 'client');
+  api.addFiles('client/z.js', 'client');
+  api.addFiles('client/main.js', 'client');
+
+  api.addFiles('server/a.js', 'server');
+  api.addFiles('server/z.js', 'server');
+  api.addFiles('server/main.js', 'server');
+
+  api.addFiles('a.js');
+  api.addFiles('z.js');
+  api.addFiles('main.js');
+});
+````
+
+Using this package as a helper, the package.js file can become this:
+
+````javascript
+var packageName = "coolguy22:some-package";
+
+Package.describe({
+  name: packageName,
+  version: '1.2.3',
+  summary: 'A summary here',
+  git: 'The git url goes here',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.3.1');
+  api.use('bjwiley2:add-all-files@0.0.1');
+  addAllFiles(api, packageName);
+});
+````
+
+## How it works
+
+Meteor-esque! The JavaScript, HTML, and CSS files in your package are loaded
+according to these rules:
+
+1. Files within a "lib" directory are loaded first
+
+2. Files within a "server" directory are loaded only to the server
+
+3. Files within a "client" directory are loaded only to the client
+
+4. Files that match main.* are loaded after everything else
+
+5. Files are loaded in alphabetical order
+
+6. The deepest directories are loaded first in depth-first-search fashion
+
+## Todo
+
+1. Provide different ways to handle Package.onTest and Package.onUse
---
+++ index.js
@@ -0,0 +1,169 @@
+var _ = require("underscore");
+var Fs = require("fs");
+var Path = require("path");
+
+if(typeof String.prototype.endsWith !== "function") {
+  String.prototype.endsWith = function (str) {
+    return this.slice(-str.length) === str;
+  };
+}
+
+if(typeof String.prototype.startsWith !== "function") {
+  String.prototype.startsWith = function (str) {
+    return this.slice(0, str.length) === str;
+  };
+}
+
+var addAllFilesApp = function () {
+  var exts = ["js", "css", "html"];
+  var contexts = null;
+
+  var main = function (api, packageName) {
+    contexts = [];
+    var relativePathToPackage = "packages" + Path.sep + packageName;
+
+    if(!process.cwd().endsWith(relativePathToPackage)) {
+      try {
+        process.chdir(relativePathToPackage);
+      }
+      catch(error) {
+        throw new Error("Could not find " + relativePathToPackage);
+        return;
+      }
+    }
+
+    loadFolder(process.cwd(), false, false, false, true);
+
+    contexts.forEach(function (context) {
+      api.addFiles(context.absolute, context.architecture);
+    });
+  };
+
+  var isValidFile = function (filename, isRoot) {
+    var valid = false;
+
+    if(isRoot && filename.endsWith("package.js")) {
+      return valid;
+    }
+
+    exts.forEach(function (ext) {
+      if(filename.endsWith("." + ext)) {
+        valid = true;
+      }
+    });
+
+    return valid;
+  };
+
+  var loadFolder = function (folder, lib, client, server, isRoot) {
+    if(client && server) {
+      throw new Error("There is either a server folder within a client folder" +
+        " or vice versa at " + folder);
+    }
+
+    var filenames=[];
+    var folderContent = Fs.readdirSync(folder);
+
+    // Load lib folder first
+    _.each(folderContent, function(filename) {
+        if(filename !== "lib") {
+          return;
+        }
+
+        var absoluteFilename = folder + Path.sep + filename;
+        var stats = Fs.statSync(absoluteFilename);
+
+        if(!stats.isDirectory()) {
+          return;
+        }
+
+        var isClient = client;
+        var isServer = server;
+        var isLib = true;
+
+        loadFolder(absoluteFilename, isLib, isClient, isServer);
+    });
+
+    // Load non lib folders
+    _.each(folderContent, function(filename) {
+        var absoluteFilename = folder + Path.sep + filename;
+        var stats = Fs.statSync(absoluteFilename);
+
+        if(stats.isDirectory() && filename !== "lib") {
+          var isClient = !server && (client || filename === "client");
+          var isServer = !client && (server || filename === "server");
+          var isLib = lib;
+
+          loadFolder(absoluteFilename, isLib, isClient, isServer);
+        }
+    });
+
+    // Load non main files
+    _.each(folderContent, function(filename) {
+        if(filename.startsWith("main")) {
+          return;
+        }
+
+        var absoluteFilename = folder + Path.sep + filename;
+        var stats = Fs.statSync(absoluteFilename);
+
+        if(!stats.isDirectory() && isValidFile(absoluteFilename, isRoot)) {
+          var context = "clientAndServer";
+
+          if(client) {
+            context = "clientOnly";
+          }
+          else if(server) {
+            context = "serverOnly";
+          }
+
+          addFile(absoluteFilename, client, server);
+        }
+    });
+
+    // Load main files
+    _.each(folderContent, function(filename) {
+        if(!filename.startsWith("main")) {
+          return;
+        }
+
+        var absoluteFilename = folder + Path.sep + filename;
+        var stats = Fs.statSync(absoluteFilename);
+
+        if(!stats.isDirectory() && isValidFile(absoluteFilename, isRoot)) {
+          var context = "clientAndServer";
+
+          if(client) {
+            context = "clientOnly";
+          }
+          else if(server) {
+            context = "serverOnly";
+          }
+
+          addFile(absoluteFilename, client, server);
+        }
+    });
+  };
+
+  var addFile = function (absolute, client, server) {
+    var architecture = undefined;
+
+    if(client) {
+      architecture = "client";
+    }
+    else if(server) {
+      architecture = "server";
+    }
+
+    contexts.push({
+      absolute: absolute,
+      architecture: architecture
+    });
+  };
+
+  return main;
+};
+
+module.exports = {
+  addAllFiles: addAllFilesApp()
+};
